8a16da05f5eb1a7e1f7f9c0ffb6e71ddd2fff95e
Shardul Sisodiya <98705625+shardul523@users.noreply.github.com>
2023-05-02 21:26:57 +0530

    Added gitignore

--- todos_util.py
+++ todos_util.py
@@ -1,10 +1,14 @@
 TODOS_FILE = "todos.txt"
 
-try:
-    file = open(TODOS_FILE, "")
-def set_todos(todos=[]):
-
-
 
 def get_todos():
+    try:
+        with open(TODOS_FILE, "r") as file:
+            return file.readlines()
+    except FileNotFoundError:
+        return []
 
+
+def set_todos(todos):
+    with open(TODOS_FILE, "w") as file:
+        file.writelines(todos)
